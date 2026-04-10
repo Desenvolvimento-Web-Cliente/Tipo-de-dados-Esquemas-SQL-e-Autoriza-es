a9b1920fa0b7a40dbe16264fd67fa1447e0194cd
Vinicius Ruza <viniciusruzamagalhaes15@gmail.com>
2026-04-10 10:37:10 -0300

    Reorganize SQL user creation and permissions queries

--- AutorizacaoSQL.sql
+++ AutorizacaoSQL.sql
@@ -1,14 +1,14 @@
---Questão 1:
+--Questão 1: Criação dos usuários
 CREATE USER User_A WITH PASSWORD='Pt3035921';
 CREATE USER User_B WITH PASSWORD='Pt3035921';
 CREATE USER User_C WITH PASSWORD='Pt3035921';
 CREATE USER User_D WITH PASSWORD='Pt3035921';
 
---Questão 2:
+--Questão 2: Privilégio de acessar e modificar as tabelas, menos a classroom, para o User_A e a possibilidade de passar esses privilégios para outros usuários
 GRANT SELECT, INSERT, UPDATE, DELETE ON SCHEMA:: dbo TO User_A WITH GRANT OPTION;
 REVOKE SELECT, INSERT, UPDATE, DELETE ON dbo.classroom TO User_A cascade;
 
---Questão 3:
+--Questão 3: Permissões do usuário A
 SELECT  princ.name, princ.type_desc
 ,       perm.permission_name
 ,       perm.state_desc
